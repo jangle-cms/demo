895926b6fd2386688c238b5c7c0f09227cf62371
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2018-09-11 18:41:26 -0500

    share with my pals!

--- README.md
+++ README.md
@@ -14,3 +14,22 @@
 1. __`npm install`__
 
 1. __`npm run dev`__
+
+
+### Quick Guide
+
+Here is a quick series of screenshots to give you a sneak peek of the UI:
+
+![Sign up screen](quick-guide/sign-up.png)
+
+![All your custom content types](quick-guide/content.png)
+
+![Blog Posts list](quick-guide/blog-posts-1.png)
+
+![Creating a new item](quick-guide/new-item.png)
+
+![Updated blog posts](quick-guide/blog-posts-2.png)
+
+![Publish button makes content live](quick-guide/publishing.png)
+
+![API endpoint ready to go!](quick-guide/live-json.png)
--- index.js
+++ index.js
@@ -6,32 +6,17 @@ jangle.start({
     live: process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://localhost:27017/jangle-live'
   },
   lists: {
-    Author: {
-      name: {
-        type: String,
-        required: true
-      },
-      contact: {
-        email: String,
-        phone: String,
-        fax: String
-      }
-    },
     'Blog Post': {
       name: {
         label: 'Title',
         type: String,
         required: true
       },
-      tags: [String],
-      date: {
-        type: Date,
-        required: false
-      },
-      authors: [{
-        type: jangle.types.relationship,
-        ref: 'Author'
-      }]
+      content: {
+        type: String,
+        required: true,
+        richText: true
+      }
     }
   }
 })
